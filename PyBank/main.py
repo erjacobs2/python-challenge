8cbf80f0610f8c1db06b1e5d208617bc4bd2968b
Emily Jacobs <emily@Emilys-MacBook-Air.local>
2018-08-26 17:26:52 -0500

    Created outline

---
+++ PyBank/main.py
@@ -0,0 +1,32 @@
+#budget_data.csv
+#calculate:
+#total number of months included in dataset
+#total net amount of "profit/losses" over the entire period
+#average change in proft/loses between months over entire period
+#greatest increase in profits (date and amount) over the entire period
+#the greatest decrease in losses (date and amount) over the entire period
+#results should print in terminal and export a text file with results
+#see instructions for how output should look
+
+#import csv file
+#define variables, determine if lists or dictionaries would help
+#determine loops or functions that will allow for calculations
+#instruct to both print in terminal and to create text file
+
+#Date and Profit/Losses columns 
+#Date is Month and Year
+#Profit and loses have no decimals, commas or dollar signs
+
+#function to count the total number of months in data
+
+#sum of numbers in Profit/losses columns
+
+#the average change between each month for all of the data (single number output)
+
+#find the biggest increase in funds in data by date and amount
+
+#find the biggest decrease in losses in data by date and amount
+
+#print terminal
+
+#create text file
