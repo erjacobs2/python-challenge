b04cb83fb246654226f12589e6a44c299e9b489c
Emily Jacobs <emily@Emilys-MacBook-Air.local>
2018-09-03 15:00:44 -0500

    sketched out more of the problems

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,29 +19,39 @@ with open(pollCSV, newline="") as csvfile:
 #Calculate complete list of candidates who received votes
 #it may not like that statement below is not tabbed/does not have new csv open
 
-def unique(list1):
-    unique_candidates = []
-    for x in list1:
-        if x not in unique_list:
-            unique_list.append(x)
-    for x in unique_candidates:
-        print(x)
+import csv
+import_candidate_list = csv.DictReader(open("election_data.csv"))
+unique_candidates = None
+#def unique(list1):
+for row in import_candidate_list:
+    candidate_list = str(row["Candidate"])
+    if unique_candidates not in candidate_list:
+       unique_candidates.append(candidate_list)
+if unique_candidates != None:
+    print(unique_candidates)
 
 #Calculate percentage of votes each candidate won
 #maybe use counter() to find the number of votes
 import csv
-import_list = csv.DictReader(open("election_data.csv"))
+import_percent_list = csv.DictReader(open("election_data.csv"))
+candidate_percent_list = None
+vote_percent_list = None
+for row in import_percent_list:
+    candidate1 = str(row["Candidate"])
+    if candidate_percent_list not in candidate1:
+        #need to pull the candidates
+        #need to calculate the number of candidate votes divide by total number of votes and multiply by 100
 
-#def getPercentages(polldata):
-    #percentagewon = (int(polldata[2])/row_count) * 100
-    #for row in csvreader:
-        #below works when there is an input - I don't know if it will find when there's no input
-        #below won't work since candidate name isn't defined
-        #if(candidatename == row[0])
-            #getPercentages(row)
+#Below may help
+#from collections import Counter
+#names = ['adam','josh','drake']
+#count = Counter(names).items()
+#percentages = {x: int(float(y) / len(names) * 100) for x, y in count}
+#for name, pct in percentages.iteritems():
+    #print '%s - %s%s' % (name, pct, '%')
 
 #Calculate total number of votes each candidate won
-#will need a way to find unique names and count each time a name appears
+#do above minus the percentage calculation
 
 #Calculate winner of election based on popular vote
 import csv
@@ -49,10 +59,10 @@ input_stuff = csv.DictReader(open("election_data.csv"))
 number_of_votes = None
 for row in input_stuff:
     candidate = str(row["Candidate"])
-    if max_income == None or max_income < income:
-        max_income = income
-        date_max_income = row["Date"]
-if max_income != None:
-    print(("The biggest increase in funds is $ %d in %s") % (max_income, date_max_income))
-else:
-    print("Uh oh!")
+    #if max_income == None or max_income < income:
+        #max_income = income
+        #date_max_income = row["Date"]
+#if max_income != None:
+    #print(("The biggest increase in funds is $ %d in %s") % (max_income, date_max_income))
+#else:
+    #print("Uh oh!")
